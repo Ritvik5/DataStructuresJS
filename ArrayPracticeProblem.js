c24aa9655e49557960893339431bac3c75cccbc6
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-18 17:15:16 +0530

    [Ritvik]Add: After sorting the array, second largest and smallest element

--- ArrayPracticeProblem.js
+++ ArrayPracticeProblem.js
@@ -2,14 +2,22 @@ console.log("Welcome To JavaScript Programming Constructs!\n");
 
 let arr = new Array();
 
+console.log("Unsorted array: ");
+
 for(let i = 0; i < 10; i++){
     arr[i] = Math.floor(Math.random() * (999-100+1) + 100);
     console.log(arr[i]);
 }
+
+console.log("\nAfter sorting: ");
+
+arr.sort();
+arr.forEach(function(element){
+    console.log(element);
+});
+
 SecondLargestNumber(arr);
-SeconSmallestNumber(arr);
-
-
+console.log("Second smallest number is : "+arr[1]);
 
 function SecondLargestNumber(arr){
     let largestNumber = arr[0];
